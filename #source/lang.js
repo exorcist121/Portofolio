1e587d21d6b13f21c66e8ce54159e60e2e05c63a
exorcist121 <12exorcist12@gmail.com>
2022-12-07 18:54:33 +0300

    langFinal

--- #source/lang.js
+++ #source/lang.js
@@ -1,22 +1,22 @@
 const langArr = {
 	"home": {
-			"ru": "главная",
+			"ru": "Главная",
 			"en": "Home",
 	}, 
 	"about": {
-			"ru": "обо мне",
+			"ru": "Обо мне",
 			"en": "About me",
 	}, 
 	"skl": {
-			"ru": "навыки",
+			"ru": "Навыки",
 			"en": "Skills",
 	}, 
 	"portfolio": {
-			"ru": "портфолио",
+			"ru": "Портфолио",
 			"en": "Portfolio",
 	}, 
 	"contacts": {
-			"ru": "контакты",
+			"ru": "Контакты",
 			"en": "Contacts",
 	}, 
 	"name": {
@@ -24,23 +24,23 @@ const langArr = {
 			"en": "Daniel <br> Komarenko",
 	}, 
 	"description": {
-			"ru": "",
+			"ru": "Фронтенд-разработчик<br>22 года, Москва",
 			"en": "Frontend-Developer<br>22 years old, Moscow",
 	}, 
 	"about-tittle": {
-		"ru": "обо мне",
+		"ru": "Обо мне",
 		"en": "About me",
 }, 
 	"about-text1": {
-		"ru": "",
+		"ru": "Привет, я Даниил – фронтенд-разработчик из Москвы.<br>Я интересуюсь разработкой и всем, что с ней связано.",
 		"en": "Hi, I'm Daniel – Frontend-developer from Moscow.<br>I'm interested in development and everything connected with it.",
 }, 
 	"about-text2": {
-		"ru": "",
+		"ru": "Я работаю в сфере веб-разработки<br> уже более трех лет.",
 		"en": "I have been working in the Web-Development<br>for more than three years.",
 }, 
 	"about-text3": {
-		"ru": "",
+		"ru": "Готов реализовывать отличные проекты<br> с замечательными людьми.",
 		"en": "Ready to implement excellent projects<br>with wonderful people.",
 }, 
 "skl-tittle": {
@@ -48,27 +48,63 @@ const langArr = {
 	"en": "Skills",
 }, 
 "skl-text": {
-	"ru": "",
-	"en": "I work in such programs as",
+	"ru": "Я работаю с такими технологиями как",
+	"en": "I work with technologies such as",
 }, 
 "portfolio-tittle": {
-	"ru": "портфолио",
+	"ru": "Портфолио",
 	"en": "Portfolio",
 }, 
+"portfolio-btn-1": {
+	"ru": "Все",
+	"en": "All",
+}, 
+"portfolio-btn-2": {
+	"ru": "Сайты на HTML",
+	"en": "HTML sites",
+}, 
+"portfolio-btn-3": {
+	"ru": "Сайты на Tilda",
+	"en": "Tilda sites",
+}, 
+"portfolio-link-1": {
+	"ru": "OJJO - Домашняя страница",
+	"en": "OJJO - Home Page",
+}, 
+"portfolio-link-2": {
+	"ru": "Finsweet - лендинг",
+	"en": "Finsweet - Landing Page",
+}, 
+"portfolio-link-3": {
+	"ru": "Dinery - лендинг",
+	"en": "Dinery - Landing Page",
+}, 
+"portfolio-link-4": {
+	"ru": "BuhOne - лендинг",
+	"en": "BuhOne - Landing Page",
+}, 
+"portfolio-link-5": {
+	"ru": "Персональный лендинг-портфолио",
+	"en": "Web - Designer - Landing Page",
+}, 
+"portfolio-link-6": {
+	"ru": "Махеев - лендинг",
+	"en": "Makheev - Landing Page",
+}, 
 "contacts-tittle": {
-	"ru": "контакты",
+	"ru": "Контакты",
 	"en": "Contacts",
 }, 
 "contacts-btn": {
-	"ru": "",
+	"ru": "Написать",
 	"en": "Send message",
 }, 
 "contacts-text": {
-	"ru": "",
+	"ru": "Хотите узнать больше или просто поболтать?<br> Добро пожаловать!",
 	"en": "Want to know more or just chat?<br>You are welcome!",
 }, 
 "socials-text": {
-	"ru": "",
+	"ru": "Свяжитесь со мной в<br>VK, Telegram, Gmail",
 	"en": "Like me on<br>VK, Telegram, Gmail",
 }, 
 }
